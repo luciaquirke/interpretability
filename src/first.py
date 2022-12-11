c41119e37e85a23f39c570744672be229a64bb4b
Lucia Quirke <luciaq@canva.com>
2022-12-11 19:49:46 +1100

    import to IntelliJ

--- src/first.py
+++ src/first.py
@@ -1,5 +1,10 @@
+import torch
+
 print("Project Ideas")
 
 print("Zak recommended re-implementing 'in-context learning and induction heads', a paper based on transformer models."
       "The first step in implementing this paper is probably implementing a transformer model. This seems like "
       "something I could achieve.")
+
+x = torch.rand(5, 3)
+print(x)
